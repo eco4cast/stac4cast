8de31e073c054ce60134f49a80e0335ad34a9298
rqthomas <rqthomas@vt.edu>
2023-02-24 11:13:41 -0500

    fleshing out a slight bit more :construction:

--- R/build_collection.R
+++ R/build_collection.R
@@ -1,8 +1,27 @@
 
 
-build_collection <- function(title, href) {
+build_collection <- function(title,
+                             links = build_links(),
+                             assets = NULL,
+                             extent) {
 
   list(id = id,
        type = "Collection",
-       title = title)
+       title = title,
+       assets = assets,  #REMEMBER TO COLLAPSE NULLS
+       )
+}
+
+build_links <- function(){
+
+
+}
+
+build_asset <- function(){
+
+
+}
+
+write_stac <- function(x, path, ...){
+ jsonlite::write_json(x, path, ...)
 }
---
+++ inst/examples/generate_catalog.R
@@ -0,0 +1,5 @@
+library(jsonlite)
+
+json_list <- build_collection()
+
+write_stac()
