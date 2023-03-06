438e313a5772bc06efa47ed8c6acd1a77c6e30da
addelany <addelany@gmail.com>
2023-03-06 20:00:23 +0000

    Add code for creating R package and remove old testing scripts

--- inst/examples/generate_catalog.R
+++ inst/examples/generate_catalog.R
@@ -1,5 +1,5 @@
 library(jsonlite)
-library(tidyvere)
+library(tidyverse)
 library(arrow)
 
 
@@ -36,14 +36,19 @@ site_id <- 'BARC'
 model_id <- 'flareGLM'
 variable_name <- 'temperature'
 
-s3 <- arrow::s3_bucket(bucket = paste0("neon4cast-forecasts/parquet/",theme,'/model_id=',model_id, '/reference_datetime=',reference_datetime),  ## this will speed up if you use above path for forecast (subset within path)
+s3 <- arrow::s3_bucket(bucket = paste0("neon4cast-forecasts/parquet/",theme),  ## this will speed up if you use above path for forecast (subset within path)
                        endpoint_override = "data.ecoforecast.org",
                        anonymous = TRUE)
 
 
 theme_df <- arrow::open_dataset(s3) %>%
-  filter(variable == variable_name, site_id == site_id)#, model_id == 'flareGLM') #%>% ##this used to just download data as quickly as possible -- need to revisit
+  filter(variable == variable_name, site_id == site_id, model_id == model_id, reference_datetime == reference_datetime) #%>% ##this used to just download data as quickly as possible -- need to revisit
 #collect()
+
+date_extent <-  arrow::open_dataset(s3) %>%
+    summarise(min = min(date),
+              max = max(date)) %>%
+    collect()
 
 description_create <- data.frame(datetime = 'ISO 8601(ISO 2019)datetime the forecast starts from (a.k.a. issue time); Only needed if more than one reference_datetime is stored in asingle file. Forecast lead time is thus datetime-reference_datetime. Ina hindcast the reference_datetimewill be earlierthan the time thehindcast was actually produced (seepubDatein Section3). Datetimesare allowed to be earlier than thereference_datetimeif areanalysis/reforecast is run before the start of the forecast period. Thisvariable was calledstart_timebefore v0.5 of theEFI standard.',
                                  site_id = 'For forecasts that are not on a spatial grid, use of a site dimension thatmaps to a more detailed geometry (points, polygons, etc.) is allowable.In general this would be documented in the external metadata (e.g., alook-up table that provides lon and lat); however in netCDF this couldbe handled by the CF Discrete Sampling Geometry data model.',
@@ -65,6 +70,13 @@ description_create <- data.frame(datetime = 'ISO 8601(ISO 2019)datetime the fore
                                  #model_id = 'unique identifier for the model used in the forecast',
                                  #date = 'ISO 8601(ISO 2019)datetime being predicted; follows CF conventionhttp://cfconventions.org/cf-conventions/cf-conventions.html#time-coordinate. This variable was called time before v0.5of the EFIconvention.For time-integrated variables (e.g., cumulative net primary productivity), one should specify thestart_datetimeandend_datetimeas two variables, instead of the singledatetime.If this is not providedthedatetimeis assumed to be the MIDPOINT of theintegrationperiod.'
 )
+
+
+## find min and max dates --update later
+# theme_df %>%
+#   # find min and max
+#   summarise(min = min(date),
+#             max = max(date))
 
 
 ##### BUILD COLLECTION BY CALLING FUNCTION
