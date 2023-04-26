c2c85ec5182cf77523d0f5a5e69463b9092ad660
Carl Boettiger <cobettig@gmail.com>
2023-04-26 07:23:42 +0000

    placeholders

--- inst/examples/minio-sync.R
+++ inst/examples/minio-sync.R
@@ -1,6 +1,12 @@
 library(minio)
 minio::mc("cp noaa.json efi/neon4cast-catalog/stac/v1/collections/noaa.json")
 
+minio::mc("cp aquatics.json efi/neon4cast-catalog/stac/v1/collections/aquatics.json")
+minio::mc("cp terrestrial.json efi/neon4cast-catalog/stac/v1/collections/terrestrial.json")
+minio::mc("cp beetles.json efi/neon4cast-catalog/stac/v1/collections/beetles.json")
+minio::mc("cp phenology.json efi/neon4cast-catalog/stac/v1/collections/phenology.json")
 
 minio::mc("cp efi-catalog.json efi/neon4cast-catalog/stac/v1/catalog.json")
 
+
+minio::mc("cp components/BONA_Twr.jpg efi/neon4cast-catalog/img/BONA_Twr.jpg")
