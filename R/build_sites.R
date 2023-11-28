6ff7da2f36dd292d6e30d184bbe41e90541f7027
addelany <addelany@gmail.com>
2023-11-28 16:16:38 -0600

    add sites

---
+++ R/build_sites.R
@@ -0,0 +1,137 @@
+#' Build the forecast or scores JSON files
+#'
+#' @param table_schema schema of data table, accessed through s3
+#' @param table_description table that includes descriptions of data columns
+#' @param start_date table that includes descriptions of data columns
+#' @param end_date table that includes descriptions of data columns
+#' @param id_value title for json file
+#' @param description_string brief description to describe JSON
+#' @param about_string string that contains the "about" asset information
+#' @param about_title title to describe the "about" asset information
+#' @param theme_title title for JSON (Forecast/Scores)
+#' @param destination_path path for saving the JSON file
+#' @param link_items link asset items for collection (child items)
+#' @param thumbnail_link link for the thumbnail image
+#' @param thumbnail_title title of thumbnail image
+#'
+#' @return JSON code for child items
+#'
+#'@export
+#'
+build_sites <- function(table_schema,
+                          table_description,
+                          start_date,
+                          end_date,
+                          id_value,
+                          description_string,
+                          about_string,
+                          about_title,
+                          theme_title,
+                          destination_path,
+                          link_items,
+                          thumbnail_link,
+                          thumbnail_title
+){
+
+  # insitu_targets_link <- config$insitu_targets_file
+  # inflow_targets_link <- config$inflow_targets_file
+  #site_link <- config$site_table_link
+  #
+  # insitu_targets_asset_description <- paste0("This R code will return results for the relevant targets file.\n\n### R\n\n```{r}\n# Use code below\n\nurl <- ",insitu_targets_link,
+  #                                            "\ntargets <- readr::read_csv(url, show_col_types = FALSE)\n```")
+  #
+  # inflow_targets_asset_description <- paste0("This R code will return results for the relevant targets file.\n\n### R\n\n```{r}\n# Use code below\n\nurl <- ",inflow_targets_link,
+  #                                            "\ntargets <- readr::read_csv(url, show_col_types = FALSE)\n```")
+  #
+  site_asset_description <- paste0("This R code will return results for the site metadata.\n\n### R\n\n```{r}\n# Use code below\n\nurl <- ",config$site_table_link,
+                                          "\nsites <- readr::read_csv(url, show_col_types = FALSE)\n```")
+
+
+  forecast_score <- list(
+    "id" = id_value,
+    "description" = description_string,
+    "stac_version"= "1.0.0",
+    "license"= "CC0-1.0",
+    "stac_extensions"= list("https://stac-extensions.github.io/scientific/v1.0.0/schema.json",
+                            "https://stac-extensions.github.io/item-assets/v1.0.0/schema.json",
+                            "https://stac-extensions.github.io/table/v1.2.0/schema.json"),
+    'type' = 'Collection',
+    'links' = c(link_items, #generate_model_items()
+                list(
+                  list(
+                    "rel" = "parent",
+                    "type"= "application/json",
+                    "href" = '../catalog.json'
+                  ),
+                  list(
+                    "rel" = "root",
+                    "type" = "application/json",
+                    "href" = '../catalog.json'
+                  ),
+                  list(
+                    "rel" = "self",
+                    "type" = "application/json",
+                    "href" = 'collection.json'
+                  ),
+                  list(
+                    "rel" = "cite-as",
+                    "href" = catalog_config$citation_doi
+                  ),
+                  list(
+                    "rel" = "about",
+                    "href" = about_string,
+                    "type" = "text/html",
+                    "title" = about_title
+                  ),
+                  list(
+                    "rel" = "describedby",
+                    "href" = catalog_config$dashboard_url,
+                    "title" = catalog_config$dashboard_title,
+                    "type" = "text/html"
+                  )
+                )),
+    "title" = theme_title,
+    "extent" = list(
+      "spatial" = list(
+        'bbox' = list(list(as.numeric(catalog_config$bbox$min_lon),
+                           as.numeric(catalog_config$bbox$min_lat),
+                           as.numeric(catalog_config$bbox$max_lon),
+                           as.numeric(catalog_config$bbox$max_lat)))),
+      "temporal" = list(
+        'interval' = list(#list(
+          #paste0(start_date,"T00:00:00Z"),
+          #paste0(end_date,"T00:00:00Z"))
+          list('pending',
+               'pending')
+        ))
+    ),
+    "table:columns" = stac4cast::build_table_columns_full_bucket(table_schema, table_description, s3_schema_build = FALSE),
+    #"table:columns" = build_table_columns_full_bucket(table_schema, table_description),
+
+    'assets' = list(
+      'data' = list(
+        "href" = config$site_table_link,
+        "type"= "application/x-parquet",
+        "title"= 'Site Metadata Access',
+        "roles" = list('data'),
+        "description"= site_asset_description
+      ),
+      'thumbnail' = list(
+        "href"= thumbnail_link,
+        "type"= "image/JPEG",
+        "roles" = list('thumbnail'),
+        "title"= thumbnail_title
+      )
+    )
+  )
+
+
+  dest <- destination_path
+  json <- file.path(dest, "collection.json")
+
+  jsonlite::write_json(forecast_score,
+                       json,
+                       pretty=TRUE,
+                       auto_unbox=TRUE)
+  stac4cast::stac_validate(json)
+}
